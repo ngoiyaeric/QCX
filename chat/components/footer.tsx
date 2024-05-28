47552f0f4b871cecd3c6f980fe42e80e4fd34973
Fabrice Renard <fabrice.renard12@outlook.com>
2024-05-28 00:45:47 -0400

    Remove social links from footer

--- chat/components/footer.tsx
+++ chat/components/footer.tsx
@@ -6,35 +6,6 @@ import { Button } from './ui/button'
 const Footer: React.FC = () => {
   return (
     <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
-      <div className="flex justify-end">
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://discord.com/invite/WEPSzPbWtq" target="_blank">
-            <SiDiscord size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://x.com/queue_cx" target="_blank">
-            <SiTwitter size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://github.com/QueueLab" target="_blank">
-            <SiGithub size={18} />
-          </Link>
-        </Button>
-      </div>
     </footer>
   )
 }
