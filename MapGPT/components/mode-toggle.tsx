c824b7d6769b177b217c0e777bbd6bb4ad48a20b
rosaleensiroosi <rosaleensiroosi.mcgill@gmail.com>
2024-06-05 13:18:26 -0400

    implement earth mode

--- MapGPT/components/mode-toggle.tsx
+++ MapGPT/components/mode-toggle.tsx
@@ -31,6 +31,9 @@ export function ModeToggle() {
         <DropdownMenuItem onClick={() => setTheme('dark')}>
           Dark
         </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('earth')}>
+          Earth
+        </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('system')}>
           System
         </DropdownMenuItem>
