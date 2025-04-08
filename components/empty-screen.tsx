bfb520125508a7fe170382d60b1af56d882a5222
EreQ <info.ericmail@gmail.com>
2025-04-08 12:38:17 +0000

    add icon to queries

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1,51 +1,59 @@
-import { Button } from '@/components/ui/button'
-import { ArrowRight } from 'lucide-react'
+import { Button } from '@/components/ui/button';
+import { TreePine, Trees, Moon, Rocket } from 'lucide-react';
 
 const exampleMessages = [
   {
     heading: 'What are the best nature parks here?',
-    message: 'What are the best nature parks here?'
+    message: 'What are the best nature parks here?',
+    icon: Trees,
   },
   {
-    heading: "What are some of the tallest trees?",
-    message: "What are some of the tallest trees?"
+    heading: 'What are some of the tallest trees?',
+    message: 'What are some of the tallest trees?',
+    icon: TreePine,
   },
   {
     heading: 'When is the next lunar eclipse?',
-    message: 'When is the next lunar eclipse?'
+    message: 'When is the next lunar eclipse?',
+    icon: Moon,
   },
   {
-    heading: 'How far is mars?',
-    message: 'How far is mars?'
-  }
-]
+    heading: 'How far is Mars?',
+    message: 'How far is Mars?',
+    icon: Rocket,
+  },
+];
+
 export function EmptyScreen({
   submitMessage,
-  className
+  className,
 }: {
-  submitMessage: (message: string) => void
-  className?: string
+  submitMessage: (message: string) => void;
+  className?: string;
 }) {
   return (
     <div className={`mx-auto w-full transition-all ${className}`}>
       <div className="bg-background p-2">
         <div className="mt-4 flex flex-col items-start space-y-2 mb-4">
-          {exampleMessages.map((message, index) => (
-            <Button
-              key={index}
-              variant="link"
-              className="h-auto p-0 text-base"
-              name={message.message}
-              onClick={async () => {
-                submitMessage(message.message)
-              }}
-            >
-              <ArrowRight size={16} className="mr-2 text-muted-foreground" />
-              {message.heading}
-            </Button>
-          ))}
+          {exampleMessages.map((item, index) => {
+            const Icon = item.icon;
+            return (
+              <Button
+                key={index}
+                variant="link"
+                className="h-auto p-0 text-base flex items-center"
+                name={item.message}
+                onClick={async () => {
+                  submitMessage(item.message);
+                }}
+              >
+                <Icon size={16} className="mr-2 text-muted-foreground" />
+                {item.heading}
+              </Button>
+            );
+          })}
         </div>
       </div>
     </div>
-  )
+  );
 }
