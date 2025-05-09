0b469d3f16fffbc98924c51f7157020bc976933a
EreQ <info.ericmail@gmail.com>
2025-05-09 09:18:58 +0000

    Invalid server actions config fix

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -35,11 +35,11 @@ export function EmptyScreen({
     <div className={`mx-auto w-full transition-all ${className}`}>
       <div className="bg-background p-2">
         <div className="mt-4 flex flex-col items-start space-y-2 mb-4">
-          {exampleMessages.map((item, index) => {
+          {exampleMessages.map((item) => {
             const Icon = item.icon;
             return (
               <Button
-                key={index}
+                key={item.message} // Use a unique property as the key.
                 variant="link"
                 className="h-auto p-0 text-base flex items-center"
                 name={item.message}
--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
-const nextConfig = {
-
-    experimental: {
-      serverActions: {
-        allowedOrigins: [
-          'localhost:3000'
-        ]
-      }
-    }
-  };
+/** @type {import('next').NextConfig} */  
+const nextConfig = {  
+  experimental: {  
+    serverActions: {  
+      allowedOrigins: ["localhost:3000", "planet.queue.cx"]  
+    },  
+  },  
+  // If you're using external packages in server components  
+  serverComponentsExternalPackages: [  
+    '@upstash/redis'  
+  ]  
+}  
   
-  export default nextConfig;
-  
+export default nextConfig
