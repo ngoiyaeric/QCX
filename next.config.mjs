6f66f119fb60b4b269fe49ce06d2776ea30894b5
EreQ <info.ericmail@gmail.com>
2025-05-13 07:34:09 +0000

    invalid server actions and key fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,9 @@
 const nextConfig = {  
   experimental: {  
     serverActions: {  
-      allowedOrigins: ["localhost:3000", "planet.queue.cx"]  
+      allowedOrigins: ["localhost:3000", "https://planet.queue.cx/"]  
     },  
   },  
-  // If you're using external packages in server components  
-  serverComponentsExternalPackages: [  
-    '@upstash/redis'  
-  ]  
-}  
-  
+};  
+
 export default nextConfig
