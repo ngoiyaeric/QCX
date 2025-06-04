e0139668a0247572975a21e657b6e732bd502279
EreQ <info.ericmail@gmail.com>
2025-06-04 12:33:03 +0000

    Update lottie-player.tsx

--- components/ui/lottie-player.tsx
+++ components/ui/lottie-player.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Lottie from 'lottie-react';
-import animationData from '@/public/images/Q.json';
+import animationData from '@/public/images/Q zoom.json';
 
 interface LottiePlayerProps {
   isVisible: boolean;
